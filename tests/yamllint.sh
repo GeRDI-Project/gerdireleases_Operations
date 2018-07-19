88e4c88f30c432c750331e603ac457710eb5a28c
Tobias Weber <tobias.weber@lrz.de>
2018-07-19 11:47:16 +0200

    fixed CI

--- tests/yamllint.sh
+++ tests/yamllint.sh
@@ -13,4 +13,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 echo "check for unvalid yml"
-yamllint --config-file tests/yamllint.conf k8s-deployment/*/*.yml
+yamllint --config-file tests/yamllint.conf */*.yml
